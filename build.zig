b73268e293709ab109dd58b19f948a7060ae11d1
aaumar25 <aaumar@pmotionf.com>
2026-07-20 11:41:55 +0900

    refactor: Require `wpcap_lib_dir` to build

--- build.zig
+++ build.zig
@@ -5,6 +5,14 @@ const Translator = @import("translate_c").Translator;
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+    const wpcap_lib_path = b.option(
+        []const u8,
+        "wpcap_lib_dir",
+        "Specify the dir to the wpcap static library artifact.",
+    ) orelse if (target.result.cpu.arch == .x86_64)
+        "vendor/wpcap/Lib/x64"
+    else
+        "vendor/wpcap/Lib";
     switch (target.result.os.tag) {
         .windows, .linux => {},
         else => return error.UnsupportedOs,
@@ -36,12 +44,17 @@ pub fn build(b: *std.Build) !void {
         .link_libc = true,
     });
     mod.addImport("soem", trans_soem.mod);
+    // Workaround for the wrong alignment caused by #pragma pack.
+    // TODO: Remove this one once the zig 0.17.0 is used. This problem might be
+    // fixed with https://codeberg.org/ziglang/translate-c/commit/174a76a5b20c0fde03032d9c1cc9d4a78a6318af
     mod.addCSourceFile(.{ .file = b.path("src/soem_shim.c") });
     mod.linkLibrary(soem.artifact("soem"));
 
-    // Workaround for wpcap library bundled in soem
+    // Building this library requires the wpcap bundled by SOEM
     if (target.result.os.tag == .windows) {
-        mod.addLibraryPath(soem.namedLazyPath("wpcap_lib_dir"));
+        const wpcap_lib: std.Build.LazyPath = .{ .cwd_relative = wpcap_lib_path };
+        mod.addObjectFile(wpcap_lib.path(b, "Packet.lib"));
+        mod.addObjectFile(wpcap_lib.path(b, "wpcap.lib"));
     }
 
     const mod_tests = b.addTest(.{
